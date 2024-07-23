fd1777d702becaebc548081b43f499815d4f5df5
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2024-07-23 18:02:23 +0530

    created backend application for myvidyalaya using springboot

---
+++ myvidyalaya/src/main/java/com/myvidyalaya/MyvidyalayaApplication.java
@@ -0,0 +1,13 @@
+package com.myvidyalaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvidyalayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyvidyalayaApplication.class, args);
+	}
+
+}
